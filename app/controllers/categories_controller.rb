7fc1936cd55404bfd390bf67cf0aff83877c60f4
Nguyễn Thắng <nguyenthang@Nguyens-MacBook-Pro.local>
2016-07-19 14:53:10 +0700

    fix bug

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,6 +1,7 @@
 class CategoriesController < ApplicationController
   def index
     @categories = Category.order(:name).where("name LIKE ?", "%#{params[:term]}")
+
     render json: @categories.map(&:name)
   end
 end
